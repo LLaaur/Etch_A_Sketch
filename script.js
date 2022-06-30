a89d6026c95008d916fc5ab6668316d991b0c394
Laur <laur_laurentiu2013@yahoo.com>
2022-06-30 19:40:38 +0300

    add reset button and black color for coloring by default

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
 const sketchPad = document.querySelector('.canvas-container');
 const sketchPadSquares = sketchPad.querySelectorAll('div');
 
+
+const paintSquare = function(color){
+    this.style.backgroundColor = 'black';
+};
+
+// GENERATING THE SQUARES OF THE SKETCHBOARD
+
+const setSize = function(size){
+
+
+    sketchPad.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    sketchPad.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    sketchPadSquares.forEach( (div)  => div.remove())
+
+    let total = size * size;
+
+    for (let i = 0; i < total; i++){
+        let square = document.createElement('div');
+        square.addEventListener('mouseover', paintSquare);
+        square.style.backgroundColor = 'white';
+        sketchPad.appendChild(square);
+    };
+};
+
+const resetSketch = function(value){
+        location.reload();
+};
+
+setSize(16);
+
+
