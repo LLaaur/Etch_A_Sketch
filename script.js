ba2093313720fbe645a268bcc02e9e3b21f73ff9
Laur <laur_laurentiu2013@yahoo.com>
2022-07-02 16:30:38 +0300

    click functionality for drawing

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 let color = 'black';
+let click = true;
 
 const colorPicker = document.getElementById('colorpicker');
 
 const paintSquare = function () {
+    if (click) {
         if (color === 'random') {
             this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
         }
         else if (color === switchColors.value) {
             this.style.backgroundColor = this.value;
         }
-        else{
+        else {
             this.style.backgroundColor = color;
         }
+    }
 };
 
 const switchColors = function (choice) {
@@ -61,3 +64,21 @@ const checkSize = function (userInput) {
         warnMsg.style.visibility = 'visible'
     }
 };
+
+// ENABLE\DISABLE DRAWING ON CLICK
+
+
+const drawMode = document.querySelector('.mode h1');
+
+document.querySelector('body').addEventListener('click', (e) => {
+    if (e.target.tagName != 'BUTTON') {
+        click = !click;
+        if (click){
+            drawMode.textContent = 'Drawing'
+        }
+        else{
+            drawMode.textContent = 'Not Drawing'
+
+        }
+    }
+});
