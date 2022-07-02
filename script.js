919b4a6421e48a08808d2a8d28b2bf1294c08411
Laur <laur_laurentiu2013@yahoo.com>
2022-07-02 13:49:25 +0300

    buttons functionality and sizing bug fix

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
-const sketchPad = document.querySelector('.canvas-container');
-const sketchPadSquares = sketchPad.querySelectorAll('div');
+let color = 'black';
 
+const colorPicker = document.getElementById('colorpicker');
 
-const paintSquare = function(color){
-    this.style.backgroundColor = 'black';
+const paintSquare = function () {
+        if (color === 'random') {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        }
+        else if (color === switchColors.value) {
+            this.style.backgroundColor = this.value;
+        }
+        else{
+            this.style.backgroundColor = color;
+        }
+};
+
+const switchColors = function (choice) {
+    color = choice;
 };
 
 // GENERATING THE SQUARES OF THE SKETCHBOARD
 
-const setSize = function(size){
+const setSize = function (size) {
 
+    let sketchPad = document.querySelector('.canvas-container');
+    let sketchPadSquares = sketchPad.querySelectorAll('div');
+
+    sketchPadSquares.forEach((div) => div.remove());
 
     sketchPad.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     sketchPad.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    sketchPadSquares.forEach( (div)  => div.remove())
-
     let total = size * size;
 
-    for (let i = 0; i < total; i++){
+    for (let i = 0; i < total; i++) {
         let square = document.createElement('div');
         square.addEventListener('mouseover', paintSquare);
         square.style.backgroundColor = 'white';
@@ -26,10 +40,25 @@ const setSize = function(size){
     };
 };
 
-const resetSketch = function(value){
-        location.reload();
-};
-
 setSize(16);
 
+// RESET BUTTON FUNCTION
+const resetSketch = function () {
+    location.reload();
+};
 
+
+const warnMsg = document.querySelector('.warning-message')
+warnMsg.style.visibility = 'hidden';
+
+
+// FUNCTION TO CHECK THE SIZE INPUT
+const checkSize = function (userInput) {
+    if (userInput >= 2 && userInput < 101) {
+        warnMsg.style.visibility = 'hidden';
+        setSize(userInput);
+    }
+    else {
+        warnMsg.style.visibility = 'visible';
+    }
+}
